9b3af4ae06095fb0daa885c14dd1a65b55ef47fb
andrii2706 <83228054+andrii2706@users.noreply.github.com>
2021-07-10 23:56:00 +0300

    redux - hm -2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import {store} from "./redux";
 import {addTodos, del_Todo, pushTodo, serLoadingTrue, setLoadingFalse} from "./redux/createActions/acrionCreators";
 import {Fragment} from "react";
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Неправильний варіант
 // const CreateTodoForm = ({onSubmit}) => {
 //     const [title, setTitle] = useState('');
 //     const [description, setDescription] = useState('');
@@ -108,6 +111,7 @@ import {Fragment} from "react";
 //
 // export default App;
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -225,3 +229,124 @@ function App() {
 }
 
 export default App;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const CreateTodoForm = ({onSubmit}) => {
+//     const [title, setTitle] = useState('');
+//     const [description, setDescription] = useState('');
+//     const [isLoading, setIsLoading] = useState(false);
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//
+//         if (!title || !description || !isLoading) return;
+//
+//         try {
+//             setIsLoading(true)
+//             await onSubmit(title, description);
+//             setTitle('')
+//             setDescription('')
+//         } catch (e) {
+//             console.log(e);
+//         } finally {
+//             setIsLoading(false)
+//         }
+//     }
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <input type="text" value={title} onChange={({target: {value}}) => setDescription(value)}
+//                    placeholder="todo title"/>
+//             <br/>
+//             <br/>
+//             <input type="text" value={description} onChange={({target: {value}}) => setDescription(value)}
+//                    placeholder="todo description"/>
+//             <br/>
+//             <br/>
+//             <button type="submit" disabled={!title || !description || isLoading}>creat to do</button>
+//         </form>
+//     )
+// }
+//
+// const Todos = ({todos, isLoading}) =>{
+//     if (isLoading) return <h1>Loading ... Your info wait 1 second</h1>
+//     return (
+//      <div>
+//          {todos.map(todo=>(
+//           <Fragment key={todo.id}>
+//               <div>{todo.title}</div>
+//               <div>{todo.description}</div>
+//               <div>Create At:{new Date(todo.createdAd).toDateString()}</div>
+//               <div>Status {todo.completed.toString()}</div>
+//           </Fragment>
+//          ))}
+//      </div>
+//     )
+// }
+//
+// function App(){
+//     const {todos, todosLoading} =useSelector(store => store.todosReducer);
+//     const dispatch =useDispatch();
+//
+// const fetchTodos = async () => {
+//     try {
+//         dispatch(serLoadingTrue())
+//         const resp = await fetch('http://localhost:8888/get-todos');
+//         const data = await resp.json();
+//
+//         dispatch(addTodos(data))
+//     } catch (e) {
+//         console.log(e);
+//     } finally {
+//         dispatch(setLoadingFalse())
+//     }
+// }
+//
+// useEffect(()=>{
+//     fetchTodos()
+// },[])
+//
+// const onTodoCreate = async (title, description) => {
+//     if (!title || !description) return;
+//     console.log(JSON.stringify({title, description}));
+//
+//     const resp = await fetch('http://localhost:8888/create-todo', {
+//         method: 'POST',
+//         body: JSON.stringify({title, description}),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//     const data = await resp.json();
+//
+//     dispatch(pushTodo(data))
+// }
+//     return(
+//         <div className="App">
+//             <CreateTodoForm onSubmit={onTodoCreate}/>
+//             <Todos todos={todos} isLoading={todosLoading}/>
+//         </div>
+//     )
+//
+//
+//
+//
+//
+//
+//
+// }
+//
+// export default App;
+//
+
+
+
+
+
+
+
+
+
+
+
+
