5d679eaf1cfac1baa3030ea76863dea24a10b7d4
andrii2706 <83228054+andrii2706@users.noreply.github.com>
2021-07-15 09:36:06 +0300

    need to make it done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 import "./App.css";
+import {MovieStates} from "./components/Context/MovieContex";
+import Heros from "./components/Hero/Heros";
 
 const App = () => {
     return (
        <div>
-
+    <MovieStates>
+        <Heros/>
+    </MovieStates>
        </div>
     );
 };
